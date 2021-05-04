a00e544e4032cefcc9f18bd50a844d911aae1af6
WRPark <jibmusae@gmail.com>
2021-05-04 18:15:31 +0900

    임시완료

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,9 @@ export default function Footer() {
   return (
     <>
       <Heading as="h1" size="md" fontFamily="noto" mb="0.75rem">
-        와이디커넥트(주)
+        와이디커넥트
       </Heading>
-      <Text>
-        대표이사 윤재원 / 사업자등록번호 421-01-02162 / 통신판매업신고번호
-        xxxxxxxxxxx
-      </Text>
+      <Text>대표이사 윤재원 / 사업자등록번호 421-01-02162</Text>
       <Text>
         부산광역시 영도구 와치로 213 / 051-403-8444 / 010-6888-8444 /
         ydconnectcorp@gmail.com
